5424420ca809f74397f30211b6d6ef0953cc166a
PaddyPan520 <Yyy520520>
2019-04-15 21:13:27 +0800

    IOS12平刷

---
+++ iOS12平刷降级工具 by Paddy/开始刷机.bat
@@ -0,0 +1,3 @@
+@echo off
+futurerestore.exe -t shsh.shsh2 --latest-baseband --latest-sep A.ipsw
+pause	#ÔÝÍ£,µÈ´ý°´¼ü¼ÌÐø
---
+++ iOS12平刷降级工具 by Paddy/系统固件下载.bat
@@ -0,0 +1,2 @@
+path=%path%; C:\Program Files\Internet Explorer\iexplore.exe
+start iexplore https://act.feng.com/wetools/index.php?r=iosRom/index
